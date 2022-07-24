de23b43a56c0e288eabadedbba784fb171c9bc99
megafudziyama@gmail.com <HydraPower3!>
2022-07-24 16:14:16 +0300

    Stuff from the lesson

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,19 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import drivers.BrowserStackMobileDriver;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static helpers.Attach.sessionId;
+import static io.qameta.allure.Allure.step;
 
 public class TestBase {
 
@@ -16,7 +26,20 @@ public class TestBase {
 
     @BeforeEach
     public void startDriver() {
+        addListener("AllureSelenide", new AllureSelenide());
 
+        open();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        String sessionId = sessionId();
+
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        step("Close driver", Selenide::closeWebDriver);
+
+        Attach.video(sessionId);
 
     }
 
